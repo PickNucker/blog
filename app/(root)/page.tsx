3890c06eb3f45a15f906c8ae6cb7014b1e29f131
PickNucker <PickNucker@gmx.de>
2025-06-21 23:49:02 +0200

    github auth

---
+++ app/(root)/page.tsx
@@ -0,0 +1,10 @@
+export default function Home() {
+  return (
+    <>
+      <section className="pink_container text-30-extrabold">
+      pink container
+      </section>
+      <h1 className="uppercase justify-center bg-black px-6 py-3 text-white sm:text-[54px] sm:leading-[64px] text-[36px] leading-[46px] max-w-5xl text-center my-5">Home</h1>
+    </>
+  )
+}
